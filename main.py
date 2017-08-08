cfc3c08bab03608e7e3f6fe5792747d9f58a4731
CowDink <sattanationmail@gmail.com>
2017-08-08 14:41:48 +0700

    fix clear not working on windows thanks to RaihanStark

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 import module.loading as ld
 import module.scene_manager as sm
 import module.character_loader as cl
+import module.os_manager as om
 import data_manager as dm
 from module.typing_text import textType as tt
 
 cur_path = os.path.dirname(__file__)
 
 def SplashLoad():
-    os.system('clear')
+    om.clear()
     print('   __                  _     _         __ _                   ')
     print('   \ \  ___  _ __ ___ | |__ | | ___   / _\ |_ ___  _ __ _   _ ')
     print('    \ \/ _ \|  _ ` _ \|  _ \| |/ _ \  \ \| __/ _ \|  __| | | |')
